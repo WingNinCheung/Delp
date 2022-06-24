e8b2b308d17f424bb215ea6efbbaa75efd1e085f
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2022-06-24 15:23:57 -0700

    created businesses and reviews db

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -52,6 +52,12 @@ module.exports = (sequelize, DataTypes) => {
 
   User.associate = function (models) {
     // associations can be defined here
+    User.hasMany(models.Review, {
+      foreignKey: "userId",
+    });
+    User.hasMany(models.Business, {
+      foreignKey: "ownerId",
+    });
   };
 
   User.prototype.toSafeObject = function () {
