867a85007cb2402969b16851659a1e2468f54f5a
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2022-06-23 20:24:06 -0700

    finished with profile dropdown and icon

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import * as sessionActions from "../../store/session";
+import "./Navigation.css";
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
@@ -31,7 +32,9 @@ function ProfileButton({ user }) {
   return (
     <>
       <button onClick={openMenu}>
-        <i class="fa-solid fa-user-vneck"></i>
+        <div className="profilex">
+          <i className="fas fa-user-alt" />
+        </div>
       </button>
       {showMenu && (
         <ul className="profile-dropdown">
