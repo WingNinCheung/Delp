7b517adfdb5e204d87da3751336c7e40ecb38db0
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2022-06-25 15:16:06 -0700

    finished loading businesses

--- frontend/src/components/BusinessDatailPage/index.js
+++ frontend/src/components/BusinessDatailPage/index.js
@@ -1,3 +1,51 @@
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 export const BusinessDetail = () => {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  let thisBusiness;
+
+  const allBusiness = useSelector((state) => state.business);
+  for (const business in allBusiness) {
+    if (id === business) {
+      thisBusiness = allBusiness[business];
+    }
+  }
+
+  return (
+    <>
+      <div
+        className="banner-session"
+        style={{
+          backgroundImage: `url(https://as1.ftcdn.net/v2/jpg/04/35/10/88/1000_F_435108880_mRcvr6sG48Ot7PPBwbK6WNmz9xaZdKtG.jpg)`,
+          height: "50%",
+          width: "100%",
+          backgroundSize: "cover",
+        }}
+      >
+        <h1 style={{ color: "white" }}>{thisBusiness.title}</h1>
+      </div>
+      <div className="detail-container">
+        <div className="photo">
+          <h2>Photo</h2>
+          <img src={thisBusiness.imageUrl} alt={`${thisBusiness.title}`}></img>
+        </div>
+        <div className="info-detail">
+          <h2>Info</h2>
+          <div className="description">
+            <h3>Description</h3>
+            {thisBusiness.description}
+          </div>
+          <div className="address">
+            <h3>Address</h3>
+            {thisBusiness.address}, {thisBusiness.city}, {thisBusiness.state},
+            {thisBusiness.ZipCode}
+          </div>
+        </div>
+        <div className="review">
+          <h2>Reviews</h2>
+        </div>
+      </div>
+    </>
+  );
 };
