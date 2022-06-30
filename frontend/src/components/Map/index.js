c62d0652cfe3bd9dd8e42c7dbf2a5aefabdc8410
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2022-06-30 13:37:42 -0700

    added map.js

--- frontend/src/components/Map/index.js
+++ frontend/src/components/Map/index.js
@@ -1,4 +1,27 @@
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import process from "process";
 import { BusinessDetail } from "../BusinessDatailPage";
+import "./map.css";
 
-export const Maps = () => {};
+export const Maps = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return (
+    <div>
+      <Map />
+    </div>
+  );
+};
+
+function Map() {
+  return (
+    <GoogleMap
+      zoom={10}
+      center={{ lat: 44, lng: -80 }}
+      mapContainerClassName="map-container"
+    ></GoogleMap>
+  );
+}
